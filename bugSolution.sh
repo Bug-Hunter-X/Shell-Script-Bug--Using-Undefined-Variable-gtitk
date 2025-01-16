7a92eb3dfbe484b8d3dd7fcf8cb6b73306159d33
BugHunter-X <askzenai@gmail.com>
2025-01-16 15:46:06 +0100

    Add bugSolution.sh file

---
+++ bugSolution.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script demonstrates the corrected version.
+
+# The variable "myVar" is assigned a value before being used.
+
+myVar="Hello, world!"
+
+echo "The value of myVar is: $myVar"
